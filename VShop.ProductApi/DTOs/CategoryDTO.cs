97fefd5dd1cab66b62faa263621c4234d2d9ffc8
adrianosantana <ans3@cin.ufpe.br>
2022-10-30 06:31:41 -0300

    feat: add automapper

---
+++ VShop.ProductApi/DTOs/CategoryDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.DTOs
+{
+    public class CategoryDTO
+    {
+        [Required(ErrorMessage = "Field name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ VShop.ProductApi/DTOs/ProductDTO.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.DTOs
+{
+    public class ProductDTO
+    {
+        [Required(ErrorMessage = "Field name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Field price is required")]
+        public decimal Price { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Field Stock is required")]
+        [Range(1, 2000)]
+        public long Stock { get; set; }
+        
+        public string? ImageURL { get; set; }
+        
+        public Category? Category { get; set; }
+    }
+}
